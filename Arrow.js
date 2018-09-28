6c93e939416f1e4d7fe0f26342be4dbab4445fda
vabnix <vabnix@hotmail.com>
2018-09-27 22:25:30 -0500

    javascript class - learning Arrow Function

--- Arrow.js
+++ Arrow.js
@@ -2,4 +2,16 @@ function printMyName(name) {
     console.log("My Name is -" + name);
 }
 
-printMyName();
+printMyName('Vaibhav');
+
+let printMyAnotherName = (name) => {
+    console.log("My Name is -" + name);
+}
+
+printMyAnotherName('Pallabi');
+
+const getNames = (firstName, secondName) => {
+    console.log("FirstName: "+ firstName + "\nSecond Name: "+ secondName);
+}
+
+getNames('Vaibhav', 'Pallavi')

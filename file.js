77cf0f4657b5ebd715a660bcfbedc6bf1686def3
vabnix <vabnix@hotmail.com>
2018-09-27 22:02:41 -0500

    javascript class - learning Let & Const

---
+++ file.js
@@ -0,0 +1,12 @@
+var myName = 'Vaibhav';
+console.log(myName);
+
+var myName = 'Pallabi';
+console.log(myName);
+
+//Now we will try to use Const and see the Output
+const fullName = 'Vaibhav Srivastava';
+console.log(fullName);
+
+fullName = 'Pallabi Chakraborty'
+console.log(fullName);//TypeError: Assignment to constant variable.
